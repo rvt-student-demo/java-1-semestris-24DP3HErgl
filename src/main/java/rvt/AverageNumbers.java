7b88dd00bb0cc444d383cbb93600a3a6fe1c829a
24DP3HErgl <A242561HE@rvt.lv>
2025-09-18 14:19:04 +0300

    Test

---
+++ src/main/java/rvt/AverageNumbers.java
@@ -0,0 +1,13 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class AverageNumbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+
+    }
+}
