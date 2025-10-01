6361ea95ba8379d1b787e4c3a45335ea2cdc3792
24DP3HErgl <A242561HE@rvt.lv>
2025-10-01 14:38:57 +0300

    uzdevumi

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
-       // Ja mēs dalam (int) ar (int)
-       // Rezultātā vienmēr būs (int)
-       int a = 7;
-       int b = 2;
-       //type-casting
-       System.out.println(
-        // a / Double.valueOf(b)
-        // a / (double) b
-        1.0 * a / b
-       );
+
+       while (true) {
+        System.out.println("Insert positive integers");
+        int number = Integer.valueOf(scanner.nextLine());
+    
+        if (number <= 0) {
+            System.out.println("Unfit number! Try again.");
+            continue;
+        }
+    
+        System.out.println("Your input was " + number);
     }
+       }
+        
+
 }
+
---
+++ src/main/java/rvt/uzdevums.java
@@ -0,0 +1,15 @@
+package rvt;
+
+public class uzdevums {
+    public static void main(String[] args) {
+
+         int number = 0;
+         while (number < 5) {
+            System.out.println(number);
+            number++;
+            // number += 1;
+         }
+
+    }
+
+}
