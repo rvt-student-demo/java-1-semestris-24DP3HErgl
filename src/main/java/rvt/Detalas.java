c4a265ad637c25df570c78f4aebe6a280ec19d64
24DP3HErgl <A242561HE@rvt.lv>
2025-10-09 15:45:06 +0300

    Sazarojumi

---
+++ src/main/java/rvt/Detalas.java
@@ -0,0 +1,11 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Detalas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+    }
+    
+}
