0660969b2745f11dc7852cf71f1d666c138de8b1
24DP3HErgl <A242561HE@rvt.lv>
2025-10-22 14:31:12 +0300

    Sum of a sequence

---
+++ src/main/java/rvt/SumSequence.java
@@ -0,0 +1,19 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class SumSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Last number?");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("The sum is " + sum);
+
+    }
+}
